e49f3a9ce924fdb1f39c77d9c4140f38c9904d6f
ousttrue <ousttrue@gmail.com>
2014-09-03 15:34:09 +0900

    SampleTcp

---
+++ SampleTcp/Program.cs
@@ -0,0 +1,141 @@
+﻿using NMPUtil.Tcp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var streamManager = new NMPUtil.Streams.StreamManager();
+
+            // setup server
+            var server = new NMPUtil.Tcp.TcpSocketListener();
+            {
+                server.AddAcceptAction(
+                stream =>
+                {
+                    var s = streamManager.AddStream(stream);
+                    Console.WriteLine("accepted");
+                    s.AddReadAction((ArraySegment<Byte> bytes) =>
+                    {
+                        Console.WriteLine(String.Format("read {0} bytes", bytes.Count));
+                    });
+                })
+                ;
+
+                server.Bind(TcpUtil.EndPoint("", 8080));
+                server.BeginAccept();
+            }
+
+            // setup client
+            var client = new NMPUtil.Tcp.TcpSocketConnector();
+            client.Connect(TcpUtil.EndPoint("127.0.0.1", 8080));
+
+            {
+                var task = client.Task
+                    .ContinueWith(t =>
+                    {
+                        streamManager.AddStream(t.Result);
+                        Console.WriteLine("connected");
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1, 2 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1, 2, 3 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1, 2, 3, 4 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1, 2, 3, 4, 5 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    ;
+
+                task.Wait();
+                Console.WriteLine("task completed");
+                Thread.Sleep(1000);
+            }
+            {
+                var task = client.Task
+                    .ContinueWith(t =>
+                    {
+                        streamManager.AddStream(t.Result);
+                        Console.WriteLine("connected");
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1, 2 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1, 2, 3 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1, 2, 3, 4 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    .ContinueWith(t =>
+                    {
+                        var bytes = new Byte[] { 1, 2, 3, 4, 5 };
+                        t.Result.Write(bytes, 0, bytes.Length);
+                        Thread.Sleep(1000);
+                        return t.Result;
+                    })
+                    ;
+
+                task.Wait();
+                Console.WriteLine("task completed");
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
--- Simple/Program.cs
+++ Simple/Program.cs
@@ -24,148 +24,22 @@ namespace Simple
         public bool MyProperty4 { get; set; }
     }
 
- 
+
     class Program
     {
         static void Main(string[] args)
         {
-            {
-                var ms = new MemoryStream();
-                var packer = new MsgPackPacker(ms); ;
-                packer.Pack(1);
+            var ms = new MemoryStream();
+            var packer = new MsgPackPacker(ms); ;
+            packer.Pack(1);
 
-                var bytes = ms.ToArray();
-                Console.WriteLine(String.Join(",", bytes));
+            var bytes = ms.ToArray();
+            Console.WriteLine(String.Join(",", bytes));
 
-                var unpacker = new MsgPackUnpacker(new ArraySegment<Byte>(bytes));
-                var result = unpacker.Unpack<int>();
+            var unpacker = new MsgPackUnpacker(new ArraySegment<Byte>(bytes));
+            var result = unpacker.Unpack<int>();
 
-                Console.WriteLine(result);
-            }
-
-            var streamManager = new NMPUtil.Streams.StreamManager();
-
-            // setup server
-            var server = new NMPUtil.Tcp.TcpSocketListener();
-            {
-                server.AddAcceptAction(
-                stream =>
-                {
-                    var s = streamManager.AddStream(stream);
-                    Console.WriteLine("accepted");
-                    s.AddReadAction((ArraySegment<Byte> bytes) =>
-                    {
-                        Console.WriteLine(String.Format("read {0} bytes", bytes.Count));
-                    });
-                })
-                ;
-
-                server.Bind(TcpUtil.EndPoint("", 8080));
-                server.BeginAccept();
-            }
-
-            // setup client
-            var client = new NMPUtil.Tcp.TcpSocketConnector();
-            client.Connect(TcpUtil.EndPoint("127.0.0.1", 8080));
-
-            {
-                var task = client.Task
-                    .ContinueWith(t =>
-                    {
-                        streamManager.AddStream(t.Result);
-                        Console.WriteLine("connected");
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1, 2 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1, 2, 3 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1, 2, 3, 4 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1, 2, 3, 4, 5 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    ;
-
-                task.Wait();
-                Console.WriteLine("task completed");
-                Thread.Sleep(1000);
-            }
-            {
-                var task = client.Task
-                    .ContinueWith(t =>
-                    {
-                        streamManager.AddStream(t.Result);
-                        Console.WriteLine("connected");
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1, 2 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1, 2, 3 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1, 2, 3, 4 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    .ContinueWith(t =>
-                    {
-                        var bytes = new Byte[] { 1, 2, 3, 4, 5 };
-                        t.Result.Write(bytes, 0, bytes.Length);
-                        Thread.Sleep(1000);
-                        return t.Result;
-                    })
-                    ;
-
-                task.Wait();
-                Console.WriteLine("task completed");
-                Thread.Sleep(1000);
-            }
+            Console.WriteLine(result);
         }
     }
 }
