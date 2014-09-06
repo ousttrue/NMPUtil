c64509e56a98ec18ad7a643393e6c149945cc277
ousttrue <ousttrue@gmail.com>
2014-09-06 17:23:33 +0900

    add nuspec

---
+++ packaging.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" NMPUtil.MsgPack.sln /p:configuration=Release
+.nuget\nuget pack NMPUtil.MsgPack\NMPUtil.MsgPack.nuspec
+.nuget\nuget pack NMPUtil.MsgPack.Rpc\NMPUtil.MsgPack.Rpc.nuspec
